8c14b40041032ed2e4c8a5a880c2694b037b586c
oluwadamme <adeniyidamilola246@gmail.com>
2021-12-16 17:21:09 +0100

    complete side bar and header UI

--- portfolio/lib/main.dart
+++ portfolio/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/constant.dart';
+import 'package:portfolio/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +15,124 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: 'Portfolio',
+      theme: ThemeData.dark().copyWith(
+          primaryColor: primaryColor,
+          scaffoldBackgroundColor: bgColor,
+          canvasColor: bgColor,
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+              .apply(bodyColor: Colors.white)
+              .copyWith(
+                  bodyText1: const TextStyle(color: bodyTextColor),
+                  bodyText2: const TextStyle(color: bodyTextColor))),
+      home: const HomeScreen(
+        children: [HomeBanner()],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomeBanner extends StatelessWidget {
+  const HomeBanner({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return AspectRatio(
+      aspectRatio: 3,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            "assets/images/bg.jpeg",
+            fit: BoxFit.cover,
+          ),
+          Container(
+            color: darkColor.withOpacity(0.66),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Discover my Awesome Art Space!',
+                  style: Theme.of(context).textTheme.headline3!.copyWith(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                MyBulidAnimatedText(),
+                SizedBox(
+                  height: defaultPadding,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Explore Now",
+                    style: TextStyle(color: darkColor),
+                  ),
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: defaultPadding * 2,
+                          vertical: defaultPadding),
+                      backgroundColor: primaryColor),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class MyBulidAnimatedText extends StatelessWidget {
+  const MyBulidAnimatedText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      style:
+          Theme.of(context).textTheme.subtitle1!.copyWith(color: Colors.white),
+      child: Row(
+        children: [
+          FlutterCodedText(),
+          SizedBox(
+            width: defaultPadding / 2,
+          ),
+          Text('I build '),
+          AnimatedTextKit(animatedTexts: [
+            TyperAnimatedText("responsible web and mobile apps."),
+            TyperAnimatedText("payment apps."),
+            TyperAnimatedText(" and contribute to open source projects.")
+          ]),
+          SizedBox(
+            width: defaultPadding / 2,
+          ),
+          FlutterCodedText(),
+        ],
+      ),
+    );
+  }
+}
+
+class FlutterCodedText extends StatelessWidget {
+  const FlutterCodedText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(TextSpan(
+      text: '<',
+      children: [
+        TextSpan(text: "flutter", style: TextStyle(color: primaryColor)),
+        TextSpan(text: '/> '),
+      ],
+    ));
   }
 }
---
+++ portfolio/lib/screens/home_screen.dart
@@ -0,0 +1,413 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:portfolio/constant.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key, required this.children}) : super(key: key);
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: maxWidth),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Expanded(flex: 1, child: SideMenu()),
+              const SizedBox(
+                width: defaultPadding,
+              ),
+              Expanded(
+                  flex: 4,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [...children],
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          const MyInfo(),
+          Expanded(
+              child: SingleChildScrollView(
+            padding: const EdgeInsets.all(defaultPadding),
+            child: Column(
+              children: [
+                AreaInfo(
+                  title: "Residence",
+                  text: 'Nigeria',
+                ),
+                AreaInfo(
+                  title: "City",
+                  text: 'Lagos',
+                ),
+                AreaInfo(
+                  title: "Age",
+                  text: '23',
+                ),
+                Skills(),
+                SizedBox(height: defaultPadding),
+                Coding(),
+                Knowledges(),
+                Divider(),
+                TextButton(
+                    onPressed: () {},
+                    child: FittedBox(
+                      child: Row(
+                        children: [
+                          Text(
+                            "DOWNLOAD CV",
+                            style: TextStyle(
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .color),
+                          ),
+                          SizedBox(width: defaultPadding / 2),
+                          SvgPicture.asset("assets/icons/download.svg")
+                        ],
+                      ),
+                    )),
+                Container(
+                  margin: EdgeInsets.only(top: defaultPadding),
+                  color: Color(0xFF24242E),
+                  child: Row(
+                    children: [
+                      Spacer(),
+                      IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset("assets/icons/linkedin.svg")),
+                      IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset("assets/icons/github.svg")),
+                      IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset("assets/icons/twitter.svg")),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.email_rounded,
+                          color: bodyTextColor,
+                        ),
+                      ),
+                      Spacer()
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class Knowledges extends StatelessWidget {
+  const Knowledges({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+          child: Text(
+            "Knowledge",
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+        ),
+        KnowlegeText(text: "Flutter, Dart"),
+        KnowlegeText(text: "Git"),
+        KnowlegeText(text: "RESTful APIs"),
+        KnowlegeText(text: "Graphql"),
+        KnowlegeText(text: "Stacked Architecture"),
+      ],
+    );
+  }
+}
+
+class KnowlegeText extends StatelessWidget {
+  const KnowlegeText({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SvgPicture.asset("assets/icons/check.svg"),
+        SizedBox(
+          width: defaultPadding / 2,
+        ),
+        Text(text),
+      ],
+    );
+  }
+}
+
+class Coding extends StatelessWidget {
+  const Coding({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+          child: Text(
+            'Coding',
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+        ),
+        AniminatedLinearProgressIndicator(
+          label: "Dart",
+          percentage: 0.7,
+        ),
+        SizedBox(width: defaultPadding),
+        AniminatedLinearProgressIndicator(
+          label: "Python",
+          percentage: 0.8,
+        ),
+        SizedBox(width: defaultPadding),
+        AniminatedLinearProgressIndicator(
+          label: "JavaScript",
+          percentage: 0.7,
+        ),
+      ],
+    );
+  }
+}
+
+class AniminatedLinearProgressIndicator extends StatelessWidget {
+  const AniminatedLinearProgressIndicator({
+    Key? key,
+    required this.label,
+    required this.percentage,
+  }) : super(key: key);
+  final String label;
+  final double percentage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: defaultPadding),
+      child: TweenAnimationBuilder(
+          tween: Tween<double>(begin: 0, end: percentage),
+          duration: defaultDuration,
+          builder: (context, double value, child) => Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        label,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        (value * 100).toInt().toString() + '%',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: defaultPadding / 2),
+                  LinearProgressIndicator(
+                    backgroundColor: darkColor,
+                    value: value,
+                    color: primaryColor,
+                  ),
+                ],
+              )),
+    );
+  }
+}
+
+class Skills extends StatelessWidget {
+  const Skills({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+          child: Text(
+            'Skills',
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+        ),
+        Row(
+          children: const [
+            Expanded(
+              child: AniminatedCircularProgressIndicator(
+                label: "Flutter",
+                percentage: 0.8,
+              ),
+            ),
+            SizedBox(width: defaultPadding),
+            Expanded(
+              child: AniminatedCircularProgressIndicator(
+                label: "Firebase",
+                percentage: 0.7,
+              ),
+            ),
+            SizedBox(width: defaultPadding),
+            Expanded(
+              child: AniminatedCircularProgressIndicator(
+                label: "NodeJs",
+                percentage: 0.5,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class AniminatedCircularProgressIndicator extends StatelessWidget {
+  final double percentage;
+  final String label;
+  const AniminatedCircularProgressIndicator({
+    Key? key,
+    required this.percentage,
+    required this.label,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        AspectRatio(
+          aspectRatio: 1,
+          child: TweenAnimationBuilder(
+              tween: Tween<double>(begin: 0, end: percentage),
+              duration: defaultDuration,
+              builder: (context, double value, child) => Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      CircularProgressIndicator(
+                        backgroundColor: darkColor,
+                        value: value,
+                        color: primaryColor,
+                      ),
+                      Center(
+                          child: Text(
+                        (value * 100).toInt().toString() + '%',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ))
+                    ],
+                  )),
+        ),
+        const SizedBox(
+          height: defaultPadding / 2,
+        ),
+        Text(
+          label,
+          style: Theme.of(context).textTheme.subtitle2,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        )
+      ],
+    );
+  }
+}
+
+class AreaInfo extends StatelessWidget {
+  const AreaInfo({
+    Key? key,
+    required this.title,
+    required this.text,
+  }) : super(key: key);
+  final String title, text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: defaultPadding / 2),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            text,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyInfo extends StatelessWidget {
+  const MyInfo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.23,
+      child: Container(
+        color: secondaryColor,
+        child: Column(
+          children: [
+            Spacer(
+              flex: 2,
+            ),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/images/dammy.JPG'),
+            ),
+            Spacer(),
+            Text(
+              "Damilola Adeniyi",
+              style: Theme.of(context).textTheme.subtitle2,
+            ),
+            Text(
+              'Flutter Developer & Software Developer',
+              style: TextStyle(fontWeight: FontWeight.w200, height: 1.5),
+              textAlign: TextAlign.center,
+            ),
+            Spacer(
+              flex: 2,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
